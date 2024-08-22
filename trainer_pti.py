3562268adf6f86ef45b8513009293845b528eefd
MLKLT <160651603+MLKLT@users.noreply.github.com>
2024-08-22 10:44:40 +0100

    Update trainer_pti.py

--- trainer_pti.py
+++ trainer_pti.py
@@ -169,7 +169,7 @@ def main(
                 hidden_size=hidden_size,
                 cross_attention_dim=cross_attention_dim,
                 rank=lora_rank,
-                device=device, # Pass the device explicitly
+                 device=torch.device("cuda" if torch.cuda.is_available() else "cpu"), # Pass the device explicitly
                 dtype=torch.float32,
             )
             unet_lora_attn_procs[name] = module
